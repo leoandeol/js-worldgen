7dfdad1a63cfc805508ffa6de2b9a77cfec2999e
Leo Andeol <leo.andeol@gmail.com>
2017-02-23 10:55:14 +0100

    Map generation finished and rendering written'

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 //GLOBALS
 var world;
+var jeu;
 const WORLD_WIDTH = 513;
 const COEFF_SCALE = 1.5;
+const LENGTH = 3;
+const WATER_RATIO = 0.3;
+var T_DYN_WATER = 0;
+const TILE_SIZE = 16;
 
 const TileType = {
     WATER : 0,
@@ -16,13 +21,18 @@ function rand(low,high){
 
 //INIT
 function init(){
+    jeu = document.getElementById("jeu");
     gen();
+    render(WORLD_WIDTH);
 }
 
 //WORLDGEN
 function gen(){
     //Fillin
     world = diamondsquare(WORLD_WIDTH);
+    world = convert(world, LENGTH,WORLD_WIDTH);
+    world = calculateWaterLevel(world,LENGTH,WORLD_WIDTH);
+    console.log(world);
 }
 
 function diamondsquare(SIZE){
@@ -47,8 +57,8 @@ function diamondsquare(SIZE){
 	{
 	    for(var y = 0; y < extent; y+=size)
 	    {
-		var sq_avg = (points[x][y].elevation + points[x+size][y].elevation + points[x][y+size].elevation + points[x+size][y+size].elevation)/4;
-		points[x+half][y+half].elevation = sq_avg + rand(-scale,scale*2);
+		var sq_avg = (points[x][y] + points[x+size][y] + points[x][y+size] + points[x+size][y+size])/4;
+		points[x+half][y+half] = sq_avg + rand(-scale,scale*2);
 	    }
 	}
 	//DIAMOND
@@ -58,36 +68,137 @@ function diamondsquare(SIZE){
 	    {
 		var avg;
 		if(y > 0){
-		    avg = (points[x][y].elevation + points[x+size][y].elevation + points[x+half][y+half].elevation + points[x+half][y-half].elevation) / 4;
+		    avg = (points[x][y]+ points[x+size][y] + points[x+half][y+half] + points[x+half][y-half]) / 4;
 		}else{
-		    avg = (points[x][y].elevation + points[x+size][y].elevation + points[x+half][y+half].elevation) / 3;
+		    avg = (points[x][y] + points[x+size][y] + points[x+half][y+half]) / 3;
 		}
-		points[x+half][y].elevation = avg + rand(-scale, scale*2);
+		points[x+half][y] = avg + rand(-scale, scale*2);
 		
 		if(y < extent - size){
-		    avg = (points[x+size][y+size].elevation + points[x+half][y+half].elevation + points[x+size][y+size].elevation + points[x+half][y+size+half].elevation) / 4;
+		    avg = (points[x+size][y+size] + points[x+half][y+half] + points[x+size][y+size] + points[x+half][y+size+half]) / 4;
 		}else{
-		    avg = (points[x+size][y+size].elevation + points[x+half][y+half].elevation + points[x+size][y+size].elevation) / 3;
+		    avg = (points[x+size][y+size] + points[x+half][y+half] + points[x+size][y+size]) / 3;
 		}
-		points[x+half][y+size].elevation = avg + rand(-scale, scale*2);
+		points[x+half][y+size] = avg + rand(-scale, scale*2);
 		
 		if(x > 0){
-		    avg = (points[x][y].elevation + points[x+half][y+half].elevation + points[x][y+size].elevation + points[x-half][y+half].elevation) / 4;
+		    avg = (points[x][y] + points[x+half][y+half] + points[x][y+size] + points[x-half][y+half]) / 4;
 		}else{
-		    avg = (points[x][y].elevation + points[x+half][y+half].elevation + points[x][y+size].elevation) / 3;
+		    avg = (points[x][y] + points[x+half][y+half] + points[x][y+size]) / 3;
 		}
-		points[x][y+half].elevation = avg + rand(-scale, scale*2);
+		points[x][y+half] = avg + rand(-scale, scale*2);
 		
 		if(x < extent - size){
-		    avg = (points[x+size][y+size].elevation + points[x+half][y+half].elevation + points[x+size][y+size].elevation + points[x+size+half][y+half].elevation) / 4;
+		    avg = (points[x+size][y+size] + points[x+half][y+half] + points[x+size][y+size] + points[x+size+half][y+half]) / 4;
 		}else{
-		    avg = (points[x+size][y+size].elevation + points[x+half][y+half].elevation + points[x+size][y+size].elevation) / 3;
+		    avg = (points[x+size][y+size] + points[x+half][y+half] + points[x+size][y+size]) / 3;
 		}
-		points[x+size][y+half].elevation = avg + rand(-scale, scale*2);
+		points[x+size][y+half] = avg + rand(-scale, scale*2);
 	    }
 	}
 	size /= 2;
 	half /= 2;
 	scale = half * COEFF_SCALE;
+    }
+
+    return points;
+}
+
+function convert(tab, length,SIZE)
+{
+    var min = 0, max = 0;
+    
+    for(var i = 0; i < SIZE; i++)
+    {
+	for(var j = 0; j < SIZE; j++)
+	{
+	    if(tab[i][j]>max){ max = tab[i][j]; }
+	    if(tab[i][j]<min){ min = tab[i][j]; }
+	}
+    }
+    
+    for(var i = 0; i < SIZE; i++)
+    {
+	for(var j = 0; j < SIZE; j++)
+	{
+	    var k = tab[i][j];
+	    k = k - min;
+	    k = (k)/(max-min);
+	    k = k * (length-1);
+	    tab[i][j] = Math.floor(k);
+	}
+    }
+    min = 0;
+    max = 0;
+    for(var i = 0; i < SIZE; i++)
+    {
+	for(var j = 0; j < SIZE; j++)
+	{
+	    if(tab[i][j]>max){ max = tab[i][j]; }
+	    if(tab[i][j]<min){ min = tab[i][j]; }
+	}
+    }
+
+    return tab;
+}
+
+function calculateWaterLevel(tab, length,SIZE)
+{
+    // let's find the value for which 2/3 of the values are lower
+    var histo = new Array();
+
+    // init the histogram
+    for(var i = 0; i < length; i++)
+    {
+	histo[i]=0;
+    }
+
+    // fill the histogram
+    for(var i = 0; i < SIZE; i++)
+    {
+	for(var j = 0; j < SIZE; j++)
+	{
+	    histo[tab[i][j]]++;
+	}
+    }
+    
+    var sum = 0;
+    var cap = (SIZE*SIZE)*WATER_RATIO;
+    for(var i = 0; i < length; i++)
+    {
+	sum+=histo[i];
+	if(sum>cap)
+	{
+	    T_DYN_WATER=i;
+	    break;
+	}
+    }
+
+    return tab;
+}
+
+function render(SIZE){
+    for(var i = 0; i < SIZE; i++)
+    {
+	for(var j = 0; j < SIZE; j++)
+	{
+	    var t = document.createElement("img");
+	    var src = "img/";
+	    switch(world[i][j]){
+	    case TileType.WATER:
+		src+="water";
+		break;
+	    case TileType.GRASS:
+		src+="grass";
+		break;
+	    case TileType.SAND:
+		src+="sand";
+		break;
+	    }
+	    src += ".png";
+	    t.src = src;
+	    t.style="width:"+(TILE_SIZE/5)+"px;height:"+(TILE_SIZE/5)+"+px;position:absolute;margin:0;top:"+(TILE_SIZE/5*j)+"px;left:"+(TILE_SIZE/5*i)+"px;";
+	    jeu.appendChild(t);
+	}
     }
 }
